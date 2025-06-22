7a421ba6f3b55d38ef5fca485ead6b1868f10a2f
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-06-22 14:46:48 -0300

    feat: extend Props interface in Input component to include InputHTMLAttributes for better type safety

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,11 +1,12 @@
 import './styles.css';
 import { Spin } from '../Spin';
+import { InputHTMLAttributes } from 'react';
 
-interface Props {
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   isLoading?: boolean;
 }
 
-export function Input({ isLoading = false, ...rest }: Props & React.InputHTMLAttributes<HTMLInputElement>) {
+export function Input({ isLoading = false, ...rest }: Props) {
   return (
     <div className="input" >
       <input type='text' {...rest} />
